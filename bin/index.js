1928740b8638384b87e7b6b10ffce0032e4c01b2
skarab42 <skarab@bluewin.ch>
2022-07-11 15:43:19 +0200

    fix: postinstall 🙈

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+if (process.env.GITHUB_ACTION) {
+  // eslint-disable-next-line no-console
+  console.log('> Skip install on GitHub Action');
+  process.exit(0);
+}
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { build } = require('../build');
 
